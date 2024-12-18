54ad84eeb34bba6e8e6713d8dd774bcd01cd9a1a
junya3 <obarajunya.0728@gmail.com>
2024-12-18 13:53:21 +0900

    ダミーの挿入

--- view/index-view.php
+++ view/index-view.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <?php
 $pageTitle = 'トップページ';
 include "../component/head.php";
 ?>
 
 <body>
-    <h1>こんにちは</h1>
+    <main>
+        <section id="recommend">
+            <h2 class="sec-title">おすすめの記事</h2>
+            <ul id="recommend-items" class="grid-col-3">
+                <li class="item">
+                    <a href="#" class="item-wrap"></a>
+                </li>
+            </ul>
+        </section>
+    </main>
+    <aside></aside>
 </body>
 
 </html>
