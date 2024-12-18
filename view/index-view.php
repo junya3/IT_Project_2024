1eb151596f6f1573057f0acdc38157e7aaa44a8c
junya3 <obarajunya.0728@gmail.com>
2024-12-18 14:19:05 +0900

    grid, subgridの実装

--- view/index-view.php
+++ view/index-view.php
@@ -9,9 +9,49 @@ include "../component/head.php";
     <main>
         <section id="recommend">
             <h2 class="sec-title">おすすめの記事</h2>
-            <ul id="recommend-items" class="grid-col-3">
+            <ul id="recommend-items">
+                <!-- ここをmap関数で管理 -->
                 <li class="item">
-                    <a href="#" class="item-wrap"></a>
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
+                </li>
+                <li class="item">
+                    <a href="#" class="item-inner">
+                        <img src="" alt="画像" width="100" height="100">
+                        <p>レシピタイトル</p>
+                        <p>10分</p>
+                    </a>
                 </li>
             </ul>
         </section>
