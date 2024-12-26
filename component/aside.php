133339599e8c07e3e6c1eb19b8ae4ed1ce1ce49b
junya3 <obarajunya.0728@gmail.com>
2024-12-27 01:38:19 +0900

    usernameを変数で管理

--- component/aside.php
+++ component/aside.php
@@ -1,3 +1,11 @@
+<?php
+// ここでユーザー名を変数で管理できればいいな…。
+$userName = 'user';
+?>
 <aside>
     <p>asideは後で作ります。user情報などをいれるのがベストかと</p>
+    <div class="user-container">
+        <img src="" alt=<?php echo $userName . 'の画像'; ?> class="user-icon" />
+        <h3 class="user-name"><?php echo $userName; ?></h3>
+    </div>
 </aside>
